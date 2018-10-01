02a6646af5f974971ab3d3b1f8b230b068f152c0
Unknown <cpriddy@gatech.edu>
2018-10-01 08:10:42 -0400

    Scene Changes
    Changes made to scene and added kupa, press space to jump

---
+++ Assets/Scripts/IdleAlternator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleAlternator : MonoBehaviour {
+    public Animator anim;
+    private float timer;
+    public float timeBetweenActions = 10f;
+	// Use this for initialization
+	void Start () {
+        anim.SetBool("IdleAction", false);
+        timer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenActions && !anim.GetBool("IdleAction"))
+        {
+            anim.SetBool("IdleAction", true);
+            timer = 0f;
+        } else if (timer >= timeBetweenActions)
+        {
+            anim.SetBool("IdleAction", false);
+            timer = 0f;
+        }
+	}
+}
---
+++ Assets/Scripts/JumpListener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpListener : MonoBehaviour {
+    public Animator anim;
+    private RaycastHit hit;
+    public float distToGround = 5f;
+    private Vector3 dir;
+    private bool isGrounded;
+
+	// Use this for initialization
+	void Start () {
+        anim.SetBool("Jump", false);
+        dir = this.transform.up * -1;
+        isGrounded = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetBool("Jump", false);
+        Debug.DrawRay(transform.position, dir * distToGround, Color.red);
+        if (Physics.Raycast(transform.position, dir, distToGround))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        if (isGrounded && (Input.GetKey(KeyCode.Space) || Input.GetButtonDown("Fire1")))
+        {
+            Debug.Log("In Jump");
+            anim.SetBool("Jump", true);
+            isGrounded = false;
+        }
+    }
+}
