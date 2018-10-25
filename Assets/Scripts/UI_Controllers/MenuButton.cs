a722d67ce9a10da4b9c4902daa5d927199f855d4
mclee3360 <mclee3360@gmail.com>
2018-10-24 21:11:19 -0400

    Create start menu

---
+++ Assets/Scripts/UI_Controllers/MenuButton.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Button extension with specialized OnSelect/OnDeselect actions.
+/// </summary>
+public class MenuButton : Button
+{
+	// Label for the button.
+	private Text label;
+
+	// Colors used for displaying selected/non-selected buttons.
+	public Color def_text_color = new Color(0.961f, 0.961f, 0.961f, 1.0f);
+	public Color select_text_color = new Color(0.059f, 0.059f, 0.059f, 1.0f);
+	private Color no_color = new Color(1f, 1f, 1f, 0f);
+
+	protected override void Start ()
+	{
+		base.Start();
+		// Get components.
+		label = GetComponentInChildren<Text>();
+		if (label == null)
+		{
+			Debug.LogError("Label not found");
+		}
+		setDefaultColors();
+	}
+
+	public override void OnSelect(BaseEventData eventData)
+	{
+		setSelectedColors();
+	}
+
+	public override void OnDeselect(BaseEventData eventData)
+	{
+		setDefaultColors();
+	}
+
+	/// <summary>
+	/// Sets the button and label's colors to the "selected" color set.
+	/// </summary>
+	private void setSelectedColors()
+	{
+		ColorBlock block = new ColorBlock();
+		block.disabledColor = no_color;
+		block.highlightedColor = def_text_color;
+		block.normalColor = def_text_color;
+		block.pressedColor = def_text_color;
+		block.colorMultiplier = 1f;
+		block.fadeDuration = 0.1f;
+		colors = block;
+		label.color = select_text_color;
+	}
+
+	/// <summary>
+	/// Sets the button and label's colors to the "non-selected" color set.
+	/// </summary>
+	private void setDefaultColors()
+	{
+		ColorBlock block = new ColorBlock();
+		block.disabledColor = no_color;
+		block.highlightedColor = no_color;
+		block.normalColor = no_color;
+		block.pressedColor = def_text_color;
+		block.colorMultiplier = 1f;
+		block.fadeDuration = 0.1f;
+		colors = block;
+		label.color = def_text_color;
+	}
+}
---
+++ Assets/Scripts/UI_Controllers/StartMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Class that contains on-click methods for the start menu buttons.
+/// </summary>
+public class StartMenuController : MonoBehaviour {
+
+	public string play_scene_name;
+	public string credits_scene_name;
+
+	/// <summary>
+	/// Starts the actual gameplay.
+	/// </summary>
+	public void play()
+	{
+		SceneManager.LoadSceneAsync(play_scene_name);
+	}
+
+	/// <summary>
+	/// Displays the controls.
+	/// </summary>
+	public void viewControls()
+	{
+		Debug.Log("Pressed \"Controls\" button");
+	}
+
+	/// <summary>
+	/// Moves to a different screen to view the credits.
+	/// </summary>
+	public void viewCredits()
+	{
+		Debug.Log("Pressed \"Credits\" button");
+	}
+
+	/// <summary>
+	/// Exits the game.
+	/// </summary>
+	public void exitGame()
+	{
+		Application.Quit();
+	}
+}
