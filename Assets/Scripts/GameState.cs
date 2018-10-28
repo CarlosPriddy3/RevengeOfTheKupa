e2ed94bb98e7fa9c580d65bfdad6b0895be103a3
mclee3360 <mclee3360@gmail.com>
2018-10-28 01:47:04 -0400

    Create state machine for scene transitions

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Game state from a scene standpoint.
+/// </summary>
+public enum State
+{
+	START,
+	PLAY,
+	WIN,
+	LOSS
+}
+
+/// <summary>
+/// Contains static variable with game's current state.
+/// </summary>
+public class GameState : MonoBehaviour
+{
+	public static State state = State.START;
+}
