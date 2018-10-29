af6570ea6d686de8fee2197b38675f497de93e54
kedarprao <kedarprao94@gmail.com>
2018-10-29 10:33:09 -0400

    Health/lives managers for kupa and marios

---
+++ Assets/Scripts/Managers/MarioHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioHealth : MonoBehaviour {
+    public int startingHealth = 100;
+    public int currentHealth;
+    public float sinkSpeed = 2.5f;
+    public int scoreValue = 10;
+
+    Animator anim;
+    AudioSource enemyAudio;
+    //ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        enemyAudio = GetComponent<AudioSource>();
+        //hitParticles = GetComponentInChildren<ParticleSystem>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        if (isSinking)
+        {
+            transform.Translate(-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage(int amount, Vector3 hitPoint)
+    {
+        if (isDead)
+            return;
+            
+        currentHealth -= amount;
+        //hitParticles.transform.position = hitPoint;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+        capsuleCollider.isTrigger = true;
+        //anim.SetTrigger("Dead");
+    }
+
+
+    public void StartSinking()
+    {
+        //GetComponent<UnityEngine.AI.NavMeshAgent>().enabled = false;
+        //GetComponent<Rigidbody>().isKinematic = true;
+        isSinking = true;
+        //ScoreManager.score += scoreValue;
+        Destroy(gameObject, 2f);
+    }
+}
---
+++ Assets/Scripts/Managers/PlayerHealth.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    //public Slider healthSlider;
+
+
+    Animator anim;
+    AudioSource playerAudio;
+    //PlayerMovement playerMovement;
+    //PlayerShooting playerShooting;
+    bool isDead;
+    bool damaged;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        //playerAudio = GetComponent<AudioSource>();
+        //playerMovement = GetComponent<PlayerMovement>();
+        //playerShooting = GetComponentInChildren<PlayerShooting>();
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //if (damaged)
+        //{
+        //    damageImage.color = flashColour;
+        //}
+        //else
+        //{
+        //    damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        //}
+        //damaged = false;
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        //healthSlider.value = currentHealth;
+        
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+    }
+
+
+    //public void RestartLevel()
+    //{
+    //    SceneManager.LoadScene(0);
+    //}
+
+}
