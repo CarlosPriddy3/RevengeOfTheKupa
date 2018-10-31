089db849572b8ff98c23b27a24c77821db485bf8
Unknown <cpriddy@gatech.edu>
2018-10-31 10:50:23 -0400

    Victory Listener
    VictoryListener

---
+++ Assets/Scripts/VictoryListener.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class VictoryListener : MonoBehaviour {
+    public float winDistance = 5f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject go in gos)
+        {
+            Debug.Log(go.name);
+        }
+        if (Vector3.Distance(gos[0].transform.position, this.transform.position) < winDistance)
+        {
+            SceneManager.LoadSceneAsync("GameEnd");
+            GameState.state = State.WIN;
+
+        }
+        
+    }
+}
