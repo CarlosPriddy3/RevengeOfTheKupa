73f0e617376d4fce49264bf56f096087f83278fa
mclee3360 <mclee3360@gmail.com>
2018-11-03 22:41:20 -0400

    Add timer functionality

---
+++ Assets/Scripts/UI_Controllers/TimerAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Encapsulates an action to perform once timer has reached zero.
+/// </summary>
+public class TimerAction : MonoBehaviour
+{
+	public string end_scene_name;
+
+	/// <summary>
+	/// The action to perform.
+	/// </summary>
+	public void onTimerEnd()
+	{
+		SceneManager.LoadSceneAsync(end_scene_name);
+		GameState.state = State.LOSS;
+	}
+}
---
+++ Assets/Scripts/UI_Controllers/TimerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Manages the game timer.
+/// </summary>
+public class TimerController : MonoBehaviour {
+
+	public Text countdown_text;
+	public int max_time;
+	public TimerAction action;
+
+	private int curr_time;
+
+	/// <summary>
+	/// Initialization
+	/// </summary>
+	void Start ()
+	{
+		curr_time = max_time;
+		StartCoroutine(StartCountdown());
+	}
+
+	/// <summary>
+	/// Coroutine that sets the timer UI to the current time remaining
+	/// every second. Once current time remaining reaches 0, performs
+	/// the specified action.
+	/// </summary>
+	public IEnumerator StartCountdown()
+	{
+		while (curr_time > 0)
+		{
+			setTimerText();
+			yield return new WaitForSeconds(1.0f);
+			curr_time--;
+		}
+		action.onTimerEnd();
+	}
+
+	/// <summary>
+	/// Sets the timer UI text with the current time remaining. If ten seconds
+	/// or less remain, text is red.
+	/// </summary>
+	private void setTimerText()
+	{
+		countdown_text.text = curr_time.ToString();
+		if (curr_time <= 10)
+		{
+			countdown_text.color = Color.red;
+		}
+	}
+}
