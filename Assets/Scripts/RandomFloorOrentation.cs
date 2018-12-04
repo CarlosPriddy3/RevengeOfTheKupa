e25f1b517d195bc6f30b5c50cd8d900c01e677ad
Unknown <cpriddy@gatech.edu>
2018-12-04 13:49:47 -0500

    Auto stash before merge of "master" and "RevengeOfTheKupa/master"

---
+++ Assets/Scripts/RandomFloorOrentation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFloorOrentation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] floors = GameObject.FindGameObjectsWithTag("floor");
+        foreach (GameObject floor in floors)
+        {
+            int random = Random.Range(1, 5);
+            floor.transform.Rotate(new Vector3(0, 90 * random, 0));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
